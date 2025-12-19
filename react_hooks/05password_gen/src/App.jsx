8b29ba38e0ba1702a572422b7dd7bc90bcd35b18
Sudeep9845 <sudeepsjoshi.dev@gmail.com>
2025-12-19 16:40:01 +0000

    chore : password generator using React hooks
    --Using useEffect to generate password on dependency change--
    --Using useCallback to memoize the password generation function--
    --Using useRef to reference the password input field for copying--

---
+++ react_hooks/05password_gen/src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+const App = () => {
+	const [password, setPassword] = useState("");
+	const [passwordLength, setPasswordLength] = useState(8);
+	const [includeNumbers, setIncludeNumbers] = useState(false);
+	const [includeSymbols, setIncludeSymbols] = useState(false);
+
+	const passwordRef = useRef(null);
+
+	const generatePassword = useCallback(() => {
+		let pass = "";
+		let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		if (includeNumbers) characters += "0123456789";
+		if (includeSymbols) characters += "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+		for (let i = 0; i < passwordLength; i++) {
+			const randomIndex = Math.floor(Math.random() * characters.length);
+			pass += characters[randomIndex];
+		}
+		setPassword(pass);
+	}, [passwordLength, includeNumbers, includeSymbols]);
+
+	const copyPassword = () => {
+		window.navigator.clipboard.writeText(password);
+		passwordRef.current?.select();
+	};
+
+	useEffect(() => {
+		generatePassword();
+	}, [passwordLength, includeNumbers, includeSymbols, generatePassword]);
+	return (
+		<div className="w-full h-screen flex flex-col justify-center items-center gap-4 bg-zinc-800">
+			<h1 className="text-3xl font-bold text-white">Password Generator</h1>
+			<div className=" w-1/4 h-fit p-4 flex gap-3">
+				<input
+					type="text"
+					name=""
+					value={password}
+					placeholder="Password"
+					className="input input-md input-accent"
+					readOnly
+					ref={passwordRef}
+				/>
+				<button
+					className="btn-accent bg-accent rounded-md px-2 text-center cursor-pointer active:scale-95"
+					onClick={copyPassword}
+				>
+					📝
+				</button>
+			</div>
+			<div className="w-1/4 h-fit p-4 flex flex-col gap-3">
+				<label htmlFor="passwordLength">Length: {passwordLength}</label>
+				<input
+					name="passwordLength"
+					type="range"
+					min={8}
+					max="30"
+					value={passwordLength}
+					onChange={(e) => setPasswordLength(e.target.value)}
+					className="range range-xs range-accent"
+				/>
+			</div>
+			<div className="w-1/4 h-fit p-4 flex gap-8">
+				<div className="flex gap-2">
+					<input
+						type="checkbox"
+						checked={includeNumbers}
+						className="checkbox checkbox-accent"
+						onChange={() => setIncludeNumbers((prev) => !prev)}
+					/>
+					<label htmlFor="">Numbers</label>
+				</div>
+				<div className="flex gap-2">
+					<input
+						type="checkbox"
+						checked={includeSymbols}
+						className="checkbox checkbox-accent"
+						onChange={() => setIncludeSymbols((prev) => !prev)}
+					/>
+					<label htmlFor="">Symbols</label>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
